163b9b09e3e62df9e1305ae20fc0589c65ec2db4
Wohlstand <admin@wohlnet.ru>
2022-03-13 00:58:29 +0300

    SDL_dynapi.h: Disable dynapi for devkitPro

--- src/dynapi/SDL_dynapi.h
+++ src/dynapi/SDL_dynapi.h
@@ -59,6 +59,8 @@
 #define SDL_DYNAMIC_API 0  /* Turn off for static analysis, so reports are more clear. */
 #elif defined(__VITA__)
 #define SDL_DYNAMIC_API 0  /* vitasdk doesn't support dynamic linking */
+#elif defined(GEKKO) || defined(__NDS__) || defined(__3DS__) || defined(__WIIU__)
+#define SDL_DYNAMIC_API 0  /* devkitPro doesn't support dynamic linking */
 #elif defined(DYNAPI_NEEDS_DLOPEN) && !defined(HAVE_DLOPEN)
 #define SDL_DYNAMIC_API 0  /* we need dlopen(), but don't have it.... */
 #endif
